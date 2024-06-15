ad55c9a26c426dca93ee27fe55381bfa771c32da
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-06-15 12:09:50 -0300

    feat: implementation dao

---
+++ src/model/dao/implementation/SellerDaoJDBC.java
@@ -0,0 +1,47 @@
+package model.dao.implementation;
+
+import java.util.List;
+
+import model.Department;
+import model.Seller;
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
